e4bb6b09f80d5da4d59aee99993e5622b3eb1276
Spencer Mitchell <smitchell556@gmail.com>
2017-03-28 13:07:33 -0700

    more thorough test of check_columns()

--- tests/test_check_columns.py
+++ tests/test_check_columns.py
@@ -10,9 +10,15 @@ import pytest
 def test_check_columns(db_and_model):
     t_class = db_and_model[1]
 
-    t_obj = t_class()
+    t_obj1 = t_class()
 
-    assert t_obj.check_columns('test_int_col')
+    assert t_obj1.check_columns('test_int_col')
 
     with pytest.raises(ValueError):
-        t_obj.check_columns('test_int_col', 'test_wrong_col')
+        t_obj1.check_columns('test_int_col', 'test_wrong_col')
+
+    t_obj2 = t_class(validate_columns=True, raise_error_on_validation=False)
+    assert not t_obj2.check_columns('test_int_col', 'test_wrong_col')
+
+    t_obj3 = t_class(False)
+    assert t_obj3.check_columns('test_int_col', 'test_wrong_col')
