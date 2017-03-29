9fc71d49084f7f5eb9d726cdd157ada198419b61
Spencer Mitchell <smitchell556@gmail.com>
2017-03-29 16:33:27 -0700

    remove test infrastructure

--- setup.py
+++ setup.py
@@ -34,9 +34,4 @@ setup(name='cuttle',
       packages=find_packages(),
       include_package_data=True,
       install_requires=['PyMySQL'],
-      setup_requires=['pytest-runner'],
-      tests_require=[
-          'pytest',
-          'pytest-cov'
-      ]
-)
+      )
