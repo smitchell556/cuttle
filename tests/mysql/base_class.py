4a06c245e4e28ea5f02b8afc131f672dcc0ea434
Spencer Mitchell <smitchell556@gmail.com>
2017-03-30 17:30:06 -0700

    create tests that check database is created

--- tests/mysql/base_class.py
+++ tests/mysql/base_class.py
@@ -7,27 +7,38 @@ import unittest
 import pymysql.cursors
 from cuttle.reef import Cuttle
 
-from ..db_credentials.mysql_credentials import USER, PASSWD
+from mysql_credentials import USER, PASSWD
 
 
 DB = '_cuttle_test_db'
+DB2 = '{}2'.format(DB)
 HOST = 'localhost'
 
 
 class DbObject(unittest.TestCase):
 
     def setUp(self):
-        self.db = Cuttle('mysql', db=DB, user=USER,
-                         passwd=PASSWD, host=HOST)
         self.con = pymysql.connect(host=HOST, user=USER, passwd=PASSWD)
-
-    def tearDown(self):
-        del self.db
 
         cur = self.con.cursor()
         cur.execute('DROP DATABASE IF EXISTS {}'.format(DB))
-
+        cur.execute('DROP DATABASE IF EXISTS {}'.format(DB2))
         cur.close()
-        self.con.close()
 
+        self.db = Cuttle('mysql', db=DB, user=USER,
+                         passwd=PASSWD, host=HOST)
+        self.db2 = Cuttle('mysql', db=DB2, user=USER,
+                          passwd=PASSWD, host=HOST)
+
+    def tearDown(self):
+        del self.db
+        del self.db2
         self.db = None
+        self.db2 = None
+
+        cur = self.con.cursor()
+        cur.execute('DROP DATABASE IF EXISTS {}'.format(DB))
+        cur.execute('DROP DATABASE IF EXISTS {}'.format(DB2))
+        cur.close()
+
+        self.con.close()
