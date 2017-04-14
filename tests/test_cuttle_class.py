985a5bcbdbe14958e7753bf7dc6970434da80329
Spencer Mitchell <smitchell556@gmail.com>
2017-04-14 16:29:16 -0700

    cleaning up tests

--- tests/test_cuttle_class.py
+++ tests/test_cuttle_class.py
@@ -2,17 +2,187 @@
 """
 Tests related to the Cuttle class.
 """
+import os
 import unittest
 
-from cuttle.reef import Cuttle
+from cuttle.reef import Cuttle, Column
+from cuttlepool import CuttlePool
+from cuttlepool.cuttlepool import PoolConnection
+
+
+DB = '_cuttle_test_db'
+DB2 = '_cuttle_test_db2'
+HOST = 'localhost'
+
+
+class BaseDbTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.credentials = dict(host=HOST)
+
+        self.sql_type = os.environ['CUTTLE_SQL'].lower()
+
+        if self.sql_type == 'mysql':
+            from mysql_credentials import USER, PASSWD
+
+            self.credentials.update(dict(user=USER, passwd=PASSWD))
+
+        self.db = Cuttle(self.sql_type, db=DB, **self.credentials)
+
+        class Heros(self.db.Model):
+            columns = [
+                Column('hero_id', 'INT'),
+                Column('hero_name', 'VARCHAR', maximum=16)
+            ]
+        self.testtable1 = Heros
+
+        self.create_heros_statement = (
+            'CREATE TABLE IF NOT EXISTS {} (\n'
+            'hero_id INT,\n'
+            'hero_name VARCHAR(16)\n'
+            ')').format(self.testtable1().name)
+        self.heros_schema = (('hero_id', 'int(11)', 'YES', '', None, ''),
+                             ('hero_name', 'varchar(16)', 'YES', '', None, ''))
+
+    def tearDown(self):
+        with self.db.Model() as drop_db:
+            drop_db.append_query('DROP DATABASE IF EXISTS {}'.format(DB))
+            drop_db.execute()
+
+    def createPool(self, **kwargs):
+        return CuttlePool(**kwargs)
+
+
+class DbNestedModelTestCase(BaseDbTestCase):
+
+    def setUp(self):
+        super(DbNestedModelTestCase, self).setUp()
+
+        class UselessTable(self.db.Model):
+            pass
+        self.uselesstable = UselessTable
+
+        class Villains(UselessTable):
+            columns = [
+                Column('villain_id', 'INT'),
+                Column('villain_name', 'VARCHAR', maximum=16)
+            ]
+        self.testtable2 = Villains
+
+        self.villains_schema = (('villain_id', 'int(11)', 'YES', '', None, ''),
+                                ('villain_name', 'varchar(16)', 'YES', '', None, ''))
+
+
+class TwoDbTestCase(BaseDbTestCase):
+
+    def setUp(self):
+        super(TwoDbTestCase, self).setUp()
+
+        self.db2 = Cuttle(self.sql_type, db=DB2, **self.credentials)
+
+        class ThrowAway(self.db2.Model):
+            columns = [
+                Column('throwaway', 'INT')
+            ]
+        self.testtable2 = ThrowAway
+
+    def tearDown(self):
+        super(TwoDbTestCase, self).tearDown()
+
+        with self.db2.Model() as drop_db:
+            drop_db.append_query('DROP DATABASE IF EXISTS {}'.format(DB2))
+            drop_db.execute()
 
 
 class CuttleInstanceTestCase(unittest.TestCase):
 
-    def test_cuttle_instantiate_mysql(self):
-        db = Cuttle('mysql', db='test_db')
-        self.assertEqual('mysql', db.Model._sql_type)
-
-    def test_cuttle_instantiate_improper_sql(self):
+    def test_improper_sql_type(self):
         with self.assertRaises(ValueError):
-            Cuttle('toxicsql', db='test_db')
+            db = Cuttle('wrongsql', db='db')
+
+    def test_no_db(self):
+        with self.assertRaises(ValueError):
+            db = Cuttle('mysql')
+
+
+class CuttleCreateDbTestCase(BaseDbTestCase):
+
+    def test_create_db(self):
+        self.db.create_db()
+
+        pool = self.createPool(db=DB, **self.credentials)
+        con = pool.get_connection()
+        cur = con.cursor()
+
+        # get databases
+        cur.execute('SHOW DATABASES')
+        dbs = cur.fetchall()
+
+        self.assertIn((DB,), dbs)
+
+        # get tables
+        cur.execute('SHOW TABLES')
+        tbls = cur.fetchall()
+
+        self.assertEqual(((self.testtable1().name,),), tbls)
+
+        # get table schema
+        cur.execute('DESCRIBE {}'.format(self.testtable1().name))
+        tblschma = cur.fetchall()
+
+        self.assertEqual(self.heros_schema, tblschma)
+
+
+class CuttleCreateMultiDbTestCase(TwoDbTestCase):
+
+    def test_create_two_dbs(self):
+        self.db.create_db()
+        self.db2.create_db()
+
+        pool1 = self.createPool(db=DB, **self.credentials)
+        pool2 = self.createPool(db=DB2, **self.credentials)
+
+        con1 = pool1.get_connection()
+        cur1 = con1.cursor()
+        con2 = pool2.get_connection()
+        cur2 = con2.cursor()
+
+        # get databases
+        cur1.execute('SHOW DATABASES')
+        dbs = cur1.fetchall()
+
+        self.assertIn((DB,), dbs)
+        self.assertIn((DB2,), dbs)
+
+        # get tables
+        cur1.execute('SHOW TABLES')
+        tbls1 = cur1.fetchall()
+        cur2.execute('SHOW TABLES')
+        tbls2 = cur2.fetchall()
+
+        self.assertEqual(((self.testtable1().name,),), tbls1)
+        self.assertEqual(((self.testtable2().name,),), tbls2)
+
+
+class CuttleCreateDbNestedModels(DbNestedModelTestCase):
+
+    def test_correct_tables_made(self):
+        self.db.create_db()
+
+        pool = self.createPool(db=DB, **self.credentials)
+        con = pool.get_connection()
+        cur = con.cursor()
+
+        # get tables
+        cur.execute('SHOW TABLES')
+        tbls = cur.fetchall()
+
+        self.assertIn((self.testtable1().name,), tbls)
+        self.assertIn((self.testtable2().name,), tbls)
+        self.assertNotIn((self.uselesstable().name,), tbls)
+
+        # get table schema
+        cur.execute('DESCRIBE {}'.format(self.testtable2().name))
+        tblschma = cur.fetchall()
+
+        self.assertEqual(self.villains_schema, tblschma)
--- tests/test_model_class.py
+++ tests/test_model_class.py
@@ -4,203 +4,66 @@ Tests related to the Model class.
 """
 import unittest
 
-from cuttle.columns import Column
-from cuttle.model import Model
+from cuttle.reef import Cuttle, Model
+from pymysql.cursors import Cursor
 
-from base_class import ModelObject, ModelSubclassObject
-
-
-class ModelNamePropertyTestCase(unittest.TestCase):
-
-    def test_model_name_lower(self):
-        model = Model()
-
-        self.assertEqual('model', model.name)
+from test_cuttle_class import BaseDbTestCase, PoolConnection
 
 
-class ModelQueryTestCase(ModelObject):
+class ModelTestCase(BaseDbTestCase):
+
+    def setUp(self):
+        super(ModelTestCase, self).setUp()
+        self.db.create_db()
+
+        self.Connection = PoolConnection
+        self.Cursor = Cursor
+
+
+class ModelNameTestCase(unittest.TestCase):
+
+    def test_name_property(self):
+        class Lower(Model):
+            pass
+
+        self.assertEqual(Lower().name, 'lower')
+
+
+class ModelConnectionTestCase(ModelTestCase):
+
+    def test_connection_property(self):
+        with self.testtable1() as heros:
+            self.assertIsInstance(heros.connection, self.Connection)
+
+    def test_cursor_property(self):
+        with self.testtable1() as heros:
+            self.assertIsInstance(heros.cursor, self.Cursor)
+
+
+class ModelQueryValuesTestCase(unittest.TestCase):
+
+    def setUp(self):
+        class TestModel(Model):
+            pass
+
+        self.Model = TestModel
 
     def test_query_property(self):
-        self.model._query = ['test', 'query']
-        self.assertEqual('test query', self.model.query)
-
-
-class ModelValuesTestCase(ModelObject):
+        with self.Model() as model:
+            query = 'this is a query'
+            model._query = query.split(' ')
+            self.assertEqual(model.query, query)
 
     def test_values_property(self):
-        self.model._values = ['test', 'values']
-        self.assertEqual(('test', 'values'), self.model.values)
+        with self.Model() as model:
+            values = ['these', 'are', 'values']
+            model._values = values
+            self.assertEqual(model.values, tuple(values))
 
-    def test_seq_of_values_peroperty(self):
-        self.model._values = [['test'], ['values']]
-        self.assertEqual([('test',), ('values',)], self.model.seq_of_values)
-
-
-class ModelSelectTestCase(ModelSubclassObject):
-
-    def test_select_no_args(self):
-        self.testtable.select()
-        self.assertEqual('SELECT * FROM testtable',
-                         self.testtable.query)
-
-    def test_select_args(self):
-        self.testtable.select('c1')
-        self.assertEqual('SELECT c1 FROM testtable',
-                         self.testtable.query)
-
-
-class ModelInsertTestCase(ModelSubclassObject):
-
-    def test_insert_no_args(self):
-        self.testtable.insert()
-        self.assertEqual('INSERT INTO testtable () VALUES ()',
-                         self.testtable.query)
-        self.assertEqual(tuple(), self.testtable.values)
-
-    def test_insert_args(self):
-        self.testtable.insert(['c1'], [1])
-        self.assertEqual('INSERT INTO testtable (c1) VALUES (%s)',
-                         self.testtable.query)
-        self.assertEqual((1,), self.testtable.values)
-
-    def test_insert_many_args(self):
-        self.testtable.insert(['c1'], [[1], [2]])
-        self.assertEqual('INSERT INTO testtable (c1) VALUES (%s)',
-                         self.testtable.query)
-        self.assertEqual([(1,), (2,)], self.testtable.seq_of_values)
-
-
-class ModelUpdateTestCase(ModelSubclassObject):
-
-    def test_update_no_args(self):
-        with self.assertRaises(ValueError):
-            self.testtable.update()
-
-    def test_update_args(self):
-        self.testtable.update(c1=1)
-        self.assertEqual('UPDATE testtable SET c1=%s',
-                         self.testtable.query)
-        self.assertEqual((1,), self.testtable.values)
-
-
-class ModelDeleteTestCase(ModelSubclassObject):
-
-    def test_delete(self):
-        self.testtable.delete()
-        self.assertEqual('DELETE FROM testtable', self.testtable.query)
-
-
-class ModelWhereTestCase(ModelSubclassObject):
-
-    def test_where_no_args(self):
-        with self.assertRaises(ValueError):
-            self.testtable.where()
-
-    def test_where_single_args(self):
-        self.testtable.where(c1=1)
-        self.assertEqual('WHERE c1=%s', self.testtable.query)
-        self.assertEqual((1,), self.testtable.values)
-
-    def test_where_multiple_args(self):
-        self.testtable.where(c1=1, c2=2)
-        try:
-            self.assertEqual('WHERE c1=%s AND c2=%s',
-                             self.testtable.query)
-            try:
-                self.assertItemsEqual((1, 2), self.testtable.values)
-            except AttributeError:
-                self.assertCountEqual((1, 2), self.testtable.values)
-        except:
-            self.assertEqual('WHERE c2=%s AND c1=%s',
-                             self.testtable.query)
-            try:
-                self.assertItemsEqual((2, 1), self.testtable.values)
-            except AttributeError:
-                self.assertCountEqual((2, 1), self.testtable.values)
-
-    def test_multiple_where(self):
-        self.testtable.where(c1=1).where(condition='or', comparison='<', c2=2)
-        self.assertEqual('WHERE c1=%s OR c2<%s', self.testtable.query)
-        try:
-            self.assertItemsEqual((1, 2), self.testtable.values)
-        except AttributeError:
-            self.assertCountEqual((1, 2), self.testtable.values)
-
-    def test_where_wrong_condition(self):
-        with self.assertRaises(ValueError):
-            self.testtable.where(condition='wrong', c1=1)
-
-    def test_where_wrong_comparison(self):
-        with self.assertRaises(ValueError):
-            self.testtable.where(comparison='wrong', c1=1)
-
-
-class ModelAppendQueryTestCase(ModelSubclassObject):
-
-    def test_append_query(self):
-        self.testtable.append_query('query')
-
-        self.assertEqual('query', self.testtable.query)
-
-
-class ModelExtendValuesTestCase(ModelSubclassObject):
-
-    def test_extend_values_list(self):
-        self.testtable.extend_values(['val1', 'val2'])
-
-        self.assertEqual(('val1', 'val2'), self.testtable.values)
-
-    def test_extend_values_list_of_lists(self):
-        self.testtable.extend_values([['val1', 'val2'], ['val3', 'val4']])
-
-        self.assertEqual([('val1', 'val2'), ('val3', 'val4')],
-                         self.testtable.seq_of_values)
-
-
-class ModelColumnsLowerTestCase(ModelSubclassObject):
-
-    def test_columns_lower_args(self):
-
-        self.assertEqual(('c1', 'c2'),
-                         self.testtable.columns_lower('C1', 'C2'))
-
-    def test_columns_lower_kwargs(self):
-
-        self.assertEqual({'c1': 1, 'c2': 'val'},
-                         self.testtable.columns_lower(C1=1, C2='val'))
-
-
-class ModelCheckColumnsTestCase(ModelSubclassObject):
-
-    def test_check_columns_valid(self):
-        testtable = self.TestTable()
-
-        self.assertTrue(testtable.check_columns('c1', 'c2'))
-
-    def test_check_columns_raise_error(self):
-        testtable = self.TestTable()
-
-        with self.assertRaises(ValueError):
-            testtable.check_columns('c3')
-
-    def test_check_columns_invalid_no_error(self):
-        testtable = self.TestTable(raise_error_on_validation=False)
-
-        self.assertFalse(testtable.check_columns('c3'))
-
-    def test_check_columns_no_validation(self):
-        testtable = self.TestTable(validate_columns=False)
-
-        self.assertTrue(testtable.check_columns('c3'))
-
-
-class ModelResetQuery(ModelSubclassObject):
-
-    def test_reset_query(self):
-        self.testtable.append_query(1)
-        self.testtable.extend_values([1])
-
-        self.testtable.reset_query()
-
-        self.assertEqual('', self.testtable.query)
-        self.assertEqual(tuple(), self.testtable.values)
+    def test_seq_of_values_property(self):
+        with self.Model() as model:
+            seq_of_values = [['these', 'are', 'values'],
+                             ['so', 'are', 'these']]
+            model._values = seq_of_values
+            self.assertEqual(model.seq_of_values,
+                             [tuple(v) for v in seq_of_values])
